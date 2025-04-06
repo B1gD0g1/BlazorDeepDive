d1a1027945194e4c888bb822374f2f9b4515d1a0
B1gD0g1 <154958364+B1gD0g1@users.noreply.github.com>
2025-04-06 15:57:36 +0800

    Blazor练习

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+
+        [Required(ErrorMessage = "名称不能为空")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "城市不能为空")]
+        public string? City { get; set; }
+    }
+}
